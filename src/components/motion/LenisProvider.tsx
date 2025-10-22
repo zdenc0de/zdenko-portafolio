6506f375b9b7f6cd3d69e55618fc5de111781dbb
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-10-22 16:37:23 -0600

    LenisProvider fixed

--- src/components/motion/LenisProvider.tsx
+++ src/components/motion/LenisProvider.tsx
@@ -4,8 +4,8 @@ import { ReactLenis } from "lenis/react";
 
 export function LenisProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2 }}>
       {children}
     </ReactLenis>
   );
-}
+}  
